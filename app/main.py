bf8049a5ef83decf321ff277f182c21ece39a2f0
LukasBaran7 <lbaran95@gmail.com>
2024-12-10 14:39:21 +0100

    Refactor requirements.txt and add python-dateutil dependency (#13)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.routers import reader
+from app.routers import reader, podcast
 from app.core.config import get_settings
 
 settings = get_settings()
@@ -21,6 +21,7 @@ app.add_middleware(
 )
 
 app.include_router(reader.router)
+app.include_router(podcast.router)
 
 @app.get("/")
 async def root():
